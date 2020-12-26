059bac1720c7cbb971cd766dea834b5b3ffe0e4d
irene <irene.nus8@gmail.com>
2020-12-26 18:15:58 +0100

    Add ui of News

--- app/src/main/java/com/example/kcalculate/ui/NewsFragment.kt
+++ app/src/main/java/com/example/kcalculate/ui/NewsFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_news.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsFragment : Fragment(){
     companion object {
@@ -19,5 +23,18 @@ class NewsFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity!!.progressBar.visibility = View.GONE
+        super.onViewCreated(view, savedInstanceState)
+        setView()
+    }
+
+    private fun setView(){
+
+        val sdf = SimpleDateFormat("E, dd MMM yyyy", Locale.ENGLISH)
+        val currentDate = sdf.format(Date())
+        tvDate.text = currentDate
     }
 }
