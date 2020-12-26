30f032e952872133294bbac4a1cbbca2077ccb86
irene <irene.nus8@gmail.com>
2020-12-26 18:16:24 +0100

    Add more parameters in classes

--- app/src/main/java/com/example/kcalculate/data/FoodEntity.kt
+++ app/src/main/java/com/example/kcalculate/data/FoodEntity.kt
@@ -11,9 +11,15 @@ db table
 @Entity(tableName = "tb_food")
 data class FoodEntity (@PrimaryKey(autoGenerate = true) var id:Int,
                        @ColumnInfo(name = "name") var name: String,
-                       @ColumnInfo(name = "kcal") var kcal: Int)
+                       @ColumnInfo(name = "kcal") var kcal: Int,
+                       @ColumnInfo(name = "carbohydrate") var carb: Int,
+                       @ColumnInfo(name = "fat") var fat: Int,
+                       @ColumnInfo(name = "protein") var protein: Int,
+                       @ColumnInfo(name = "sugar") var sugar: Int,
+                       @ColumnInfo(name = "salt") var salt: Int,
+                       @ColumnInfo(name = "fiber") var fiber: Int)
 {
-    constructor():this(0,"",0)
+    constructor():this(0,"",0,0,0,0,0,0,0)
     companion object{
         fun getEntity(pInfo: Food): FoodEntity
         {
@@ -22,6 +28,12 @@ data class FoodEntity (@PrimaryKey(autoGenerate = true) var id:Int,
             foodEntity.id = pInfo.id
             foodEntity.name = pInfo.name
             foodEntity.kcal = pInfo.kcal
+            foodEntity.carb = pInfo.carbohydrate
+            foodEntity.fat = pInfo.fat
+            foodEntity.protein = pInfo.protein
+            foodEntity.sugar = pInfo.sugar
+            foodEntity.salt = pInfo.salt
+            foodEntity.fiber = pInfo.fiber
             return foodEntity
         }
     }
--- app/src/main/java/com/example/kcalculate/model/Food.kt
+++ app/src/main/java/com/example/kcalculate/model/Food.kt
@@ -3,7 +3,13 @@ package com.example.kcalculate.model
 data class Food(
     var id: Int,
     var name: String,
-    var kcal: Int
+    var kcal: Int,
+    var carbohydrate: Int,
+    var fat: Int,
+    var protein: Int,
+    var sugar: Int,
+    var salt: Int,
+    var fiber: Int
 ) {
-    constructor() : this(0, "", 0) {}
+    constructor() : this(0, "", 0,0,0,0,0,0,0) {}
 }
